b641c6fc779ec12ab9edf976c28865bc2b9b458d
rampmail29 <113305123+rampmail29@users.noreply.github.com>
2025-05-28 15:42:31 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { LogBox, Alert } from 'react-native';
 
 const App = () => {
+  useEffect(() => {
+    console.log('✅ App cargada');
+    Alert.alert('Info', 'App cargada');
+  }, []);
+
+  // Opcional: ignora advertencias que no afectan ejecución
+  LogBox.ignoreLogs([
+    'Non-serializable values were found in the navigation state',
+  ]);
+
   return (
     <AuthProvider>
       <SafeAreaProvider>
